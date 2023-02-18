b03b9f7b004a436b111f6f215f3a7b91c5d2f534
Narendra <nkvidda@gmail.com>
2023-02-18 18:29:37 +0530

    tailwindcss working setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    content: [  "./screens/*.{js,jsx,ts,tsx}", 
+                "./Pages/*.{js,jsx,ts,tsx}",
+                "./Components/*.{js,jsx,ts,tsx}"
+            ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
